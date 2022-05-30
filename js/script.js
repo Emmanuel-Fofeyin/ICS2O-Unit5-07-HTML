488116af057ae82a7c2a3407060c9972b2ce6921
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-30 03:19:02 +0000

    we good

--- js/script.js
+++ js/script.js
@@ -22,14 +22,14 @@ function myButtonClicked() {
   // input
   var firstInteger = document.getElementById("integer-one").value
 
- // process and output
+  // process and output
   var total = 0
   if (firstInteger < 0) {
     document.getElementById("loop").innerHTML =
       "Please input a positive number."
   } else {
     for (let counter = 0; counter <= firstInteger; counter ++) {
-       total += counter
+      total += counter
     }
   }
   document.getElementById("loop").innerHTML = "The number is " + total
